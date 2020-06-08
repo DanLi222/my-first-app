8fa8761a95716292c2db03c7eddb68150cea6f99
DanLi222 <anne.leedan@gmail.com>
2020-06-08 15:27:41 -0400

    Restructured dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,64 +8,50 @@ import * as CanvasJS from '../canvasjs.min';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  data$;
   global$;
   topCountries$;
 
   constructor(
     private configService: ConfigService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    
-    this.configService.fetchData()
-        .subscribe( res => {
-          console.log('data ' + JSON.stringify(res));
-          this.data$ = res;
-      });
-
-    this.configService.fetchCountries()
-        .subscribe((res: any) => {
-          this.global$ = res.Global;
-          this.topCountries$ = res.Countries.sort((a, b) => {
-            return a.TotalConfirmed < b.TotalConfirmed ? 1 : -1;
-          }).slice(0, 10);
-          // const countrynumbers = [];
-          // for (const country of this.topCountries$) {
-          //   countrynumbers.push(
-          //     {
-          //       y: country.TotalConfirmed,
-          //       label: country.Country
-          //     }
-          //   );
-          // }
-
-          const chart = new CanvasJS.Chart('chartContainer', {
-            animationEnabled: true,
-            exportEnabled: true,
-            title: {
-              text: 'Top 10 Countries'
-            },
-            data: [{
-              type: 'column',
-              dataPoints: [
-                { y: this.topCountries$[0].TotalConfirmed, label: this.topCountries$[0].Country },
-                { y: this.topCountries$[1].TotalConfirmed, label: this.topCountries$[1].Country },
-                { y: this.topCountries$[2].TotalConfirmed, label: this.topCountries$[2].Country },
-                { y: this.topCountries$[3].TotalConfirmed, label: this.topCountries$[3].Country },
-                { y: this.topCountries$[4].TotalConfirmed, label: this.topCountries$[4].Country },
-                { y: this.topCountries$[5].TotalConfirmed, label: this.topCountries$[5].Country },
-                { y: this.topCountries$[6].TotalConfirmed, label: this.topCountries$[6].Country },
-                { y: this.topCountries$[7].TotalConfirmed, label: this.topCountries$[7].Country },
-                { y: this.topCountries$[8].TotalConfirmed, label: this.topCountries$[8].Country },
-                { y: this.topCountries$[9].TotalConfirmed, label: this.topCountries$[9].Country }
-              ]
-            }]
-          });
-          chart.render();
-        });
+    this.showGlobal();
   }
 
+  showGlobal() {
+    this.configService.fetchCountries()
+      .subscribe((res: any) => {
+        this.global$ = res.Global;
+        this.topCountries$ = res.Countries.sort((a, b) => {
+          return a.TotalConfirmed < b.TotalConfirmed ? 1 : -1;
+        }).slice(0, 10);
+        this.showCountries();
+      });
+  }
 
+  showCountries() {
+    const countrynumbers = [];
+    for (const country of this.topCountries$) {
+      countrynumbers.push(
+        {
+          y: country.TotalConfirmed,
+          label: country.Country
+        }
+      );
+    }
+    const chart = new CanvasJS.Chart('chartContainer', {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: 'Top 10 Countries'
+      },
+      data: [{
+        type: 'column',
+        dataPoints: countrynumbers
+      }]
+    });
+    chart.render();
+  }
 
 }
