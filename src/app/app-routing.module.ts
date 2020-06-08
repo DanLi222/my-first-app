0f459deb351aa777a43627a2d4a0b08a2ff04c20
DanLi222 <anne.leedan@gmail.com>
2020-06-08 12:56:38 -0400

    Added default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {LoginComponent} from './login/login.component';
 
 
 const routes: Routes = [
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent,
