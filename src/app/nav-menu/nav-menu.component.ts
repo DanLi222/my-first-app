4025718eab3e84a1955c1f96c538144ff6eaaaa6
DanLi222 <anne.leedan@gmail.com>
2020-06-08 14:47:45 -0400

    Moved login to nav-menu

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {AuthService, SocialUser} from 'angularx-social-login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+  user: SocialUser;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +19,23 @@ export class NavMenuComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    });
+  }
+
+  signOut(): void {
+    this.authService.signOut().then(success => this.router.navigate(['/login']));
+  }
 }
